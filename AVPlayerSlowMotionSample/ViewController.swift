a2360140d9e01a54791410d5d9357b67c9e5c14d
kozyty <kozyty@gmail.com>
2016-12-03 21:23:55 +0900

    Initial Commit

---
+++ AVPlayerSlowMotionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVPlayerSlowMotionSample
+//
+//  Created by Taiyo Kojima on 2016/12/03.
+//  Copyright © 2016年 Taiyo Kojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
